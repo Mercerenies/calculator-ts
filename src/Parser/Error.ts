706ad447f01ca4ddbf4cdc58f8538e6669595ba3
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-10-17 15:36:03 -0400

    ParseError.toString and joining errors

--- src/Parser/Error.ts
+++ src/Parser/Error.ts
@@ -2,18 +2,41 @@
 export class ParseError {
   readonly pos: number;
   readonly message: string;
+  readonly expecting: string[];
 
-  constructor(pos: number, message: string) {
+  constructor(pos: number, message: string, expecting: string[]) {
     this.pos = pos;
     this.message = message;
+    this.expecting = expecting;
+  }
+
+  toString(): string {
+    let base = `Parse error at ${this.pos}:`;
+    if (this.message == "")
+      base += ` Expecting ${this.expecting.join(', ')}`;
+    else if (this.expecting.length == 0)
+      base += this.message;
+    else
+      base += ` ${this.message} (Expecting ${this.expecting.join(', ')})`;
+    return base;
+  }
+
+  static join(err: ParseError[]): ParseError {
+    if (err.length == 0)
+      return new ParseError(0, "", []);
+    return err.reduce((a, b) => {
+      return new ParseError(Math.min(a.pos, b.pos),
+                            a.message + ", " + b.message,
+                            a.expecting.concat(b.expecting));
+    });
   }
 
 }
 
 export function fail(pos: number, message: string): ParseError {
-  return new ParseError(pos, message);
+  return new ParseError(pos, message, []);
 }
 
 export function expecting(pos: number, values: string[]): ParseError {
-  return fail(pos, "Expecting " + values.join(", "));
+  return new ParseError(pos, "", values);
 }
