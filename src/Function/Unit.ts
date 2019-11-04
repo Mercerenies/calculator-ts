46eb636015456b0616f7156b2669c0f717ad0fef
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-11-04 18:13:53 -0500

    Applied the unit parser and made a 2-argument form for uconvert

--- src/Function/Unit.ts
+++ src/Function/Unit.ts
@@ -2,28 +2,31 @@
 import { Function } from './Function'
 import FunctionBuilder from './Builder'
 import Unit from '../Unit/Unit'
-import Dimension from '../Unit/Dimension'
 import * as Table from '../Unit/Table'
+import { parseUnits } from '../Unit/Parse'
 import Expr from '../Expr'
+import * as Compound from '../Compound'
 
 function convertUnits(args: Expr[]): Expr | null {
+
+  if (args.length === 2)
+    args = [Expr.from(1), ...args];
+
   if (args.length !== 3)
     return null;
 
   const [value, ufrom, uto] = args;
-  let fromUnit: Unit | undefined = undefined;
-  let toUnit: Unit | undefined = undefined;
+  const fromUnit = parseUnits(ufrom);
+  const toUnit = parseUnits(uto);
 
-  ufrom.ifVar((s) => {
-    fromUnit = Table.lookupUnit(s);
-  });
-  uto.ifVar((s) => {
-    toUnit = Table.lookupUnit(s);
-  });
-  if (fromUnit === undefined || toUnit === undefined)
+  if (fromUnit === null || toUnit === null)
     return null;
 
-  return Unit.convert(fromUnit, toUnit, value);
+  const value1 = Compound.binDiv(
+    Compound.binMul(value, Expr.from(fromUnit.coefficient)),
+    Expr.from(toUnit.coefficient)
+  );
+  return Unit.convert(fromUnit.unit, toUnit.unit, value1);
 
 }
 
